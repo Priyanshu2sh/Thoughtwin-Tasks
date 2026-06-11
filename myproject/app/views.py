fbca0e2315f430d220968345c8b606bc991377e0
Priyanshu Sharma <ps200410@gmail.com>
2026-06-11 15:21:04 +0530

    small changes

--- myproject/app/views.py
+++ myproject/app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# python manage.py makemigrations
+
+# python manage.py makemigrations myapp
+
+# pythono manage.py migrate
+
+# python manage.py migrate myapp
+
+# python manage.py showmigrations
+
+# python manage.py createsuperuser
+
+# python manage.py shell
+
