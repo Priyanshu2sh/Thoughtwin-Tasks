2ea95943c67a0d7670d071b97c9a2fe209beeb64
Priyanshu Sharma <ps200410@gmail.com>
2026-06-12 19:09:24 +0530

    fields and fieldsets

--- myproject/app/admin.py
+++ myproject/app/admin.py
@@ -11,3 +11,23 @@ for model in models:
         admin.site.register(model)
     except admin.sites.AlreadyRegistered:
         pass
+
+
+
+# class PostAdmin(admin.ModelAdmin):
+#     fields = ['title', ('author', 'slug')]
+
+
+# class PostAdmin(admin.ModelAdmin):
+#     TEXT = 'Some description text'
+#     fieldsets = (
+#         ('Section 1', {
+#             'fields': ('title', 'author'),
+#             'description': '%s' % TEXT,
+#         }),
+#         ('Section 2', {
+#             'fields': ('slug',),
+#         }),
+#     )
+
+# admin.site.register(Post, PostAdmin)
