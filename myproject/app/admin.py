514a19e47064cb079730aaa1cf210f194bf78205
Priyanshu Sharma <ps200410@gmail.com>
2026-06-15 11:53:57 +0530

    filters

--- myproject/app/admin.py
+++ myproject/app/admin.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 import django.apps
+from .models import Post, Profile
+from django import  forms
 # Register your models here.
 
 models = django.apps.apps.get_models()
 
-print(models)
+# print(models)
 
-for model in models:
-    try:
-        admin.site.register(model)
-    except admin.sites.AlreadyRegistered:
-        pass
+# for model in models:
+#     try:
+#         admin.site.register(model)
+#     except admin.sites.AlreadyRegistered:
+#         pass
 
 
 
@@ -31,3 +33,25 @@ for model in models:
 #     )
 
 # admin.site.register(Post, PostAdmin)
+
+
+
+# class PostForm(forms.ModelForm):
+#     def __init__(self, *args, **kwargs):
+#         super(PostForm, self).__init__(*args, **kwargs)
+#         self.fields['title'].help_text = 'New Help Text'
+
+#     class Meta:
+#         model = Post
+#         exclude = ('',)
+
+# class PostFormAdmin(admin.ModelAdmin):
+#     form = PostForm
+
+# admin.site.register(Post, PostFormAdmin)
+
+class Filter(admin.ModelAdmin):
+    list_display = ('id', 'email', 'created_at', 'role', 'is_active')
+    list_filter = ('is_active', 'role', 'created_at')
+
+admin.site.register(Profile, Filter)
